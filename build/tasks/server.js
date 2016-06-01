06716c562f1aaa7b02829a8512fa4c522c0afec2
Matt Huynh <liem.tri.huynh@avanade.com>
2016-05-31 17:12:14 -0700

    Fail the build task for any compile error

--- build/tasks/server.js
+++ build/tasks/server.js
@@ -3,13 +3,23 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var paths = require('../paths');
+var Q = require('q');
 
 var tsProject = ts.createProject('tsconfig.json', {
     typescript: require('typescript')
 });
 
 gulp.task('build', [], function () {
-    return gulp.src(paths.buildserver.ts)
+    var defer = Q.defer();
+    var tsResult = gulp.src(paths.buildserver.ts)
         .pipe(ts(tsProject))
-        .pipe(gulp.dest(paths.buildserver.output));
+        .on("error", function(err){ 
+            defer.reject(err);
+        })
+        .pipe(gulp.dest(paths.buildserver.output))
+        .on("end", function(){
+            defer.resolve(tsResult);
+        });
+        
+    return defer.promise;
 });
