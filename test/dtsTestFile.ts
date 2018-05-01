92cd3ad4542101bee67147bede724f6b1a61d781
Andrew Benton <andrew.j.benton@avanade.com>
2018-05-01 14:45:12 -0700

    Add tests for new functionality

---
+++ test/dtsTestFile.ts
@@ -0,0 +1,6 @@
+/** dts:core_company,
+ * sys_user,
+ * cmdb_ci_win_server
+ */
+
+ export const words: string = "these are words.";
--- test/test.ts
+++ test/test.ts
@@ -1,18 +1,47 @@
-/**dts: core_company */
+/** dts: core_company */
+import * as assert from "assert";
+import Project, { SyntaxKind } from "ts-simple-ast";
 
-export class MyTest {
-    private myfunc() {
-        const fooname = 'cmdb_ci';
-        const foo2 = new GlideRecord(fooname);
-        var foo = new GlideRecord('cmdb');
-    }
+describe("DTS Parser", () => {
+    it("should return types from a multiline JSDoc dts declaration", () => {
+        const types = readDtsTypesFromMultiLineJSDocCommentBlock();
 
-    public somefunc() {
-        const foo = new GlideRecord('cmdb_ci');
+        assert.equal(types.length, 3);
+        ["core_company", "sys_user", "cmdb_ci_win_server"].forEach((type, i) => {
+            assert.equal(type, types[i]);
+        });
+    });
+});
 
-        const foo2 = new GlideRecord('cmdb_ci_win_server');
-        if(new Number(foo2.operational_status) == 7){
+function readDtsTypesFromMultiLineJSDocCommentBlock(){
+    let types: Array<string> = [];
 
-        }
-    }
+    const project = new Project({compilerOptions: {removeComments: false}});
+    project.addExistingSourceFiles("**/*dtsTestFile.ts");
+    project.getSourceFiles()
+            .forEach(file => {
+                // Find all dts comment defs: /**dts: tablename1,tablename2 */
+                // support multiline
+                const dtsregex = new RegExp(/dts:[\s+]?([\w,\d\s\r\n\*]+)/gi);
+
+                const comments = file.getDescendantsOfKind(SyntaxKind.JSDocComment);
+                comments.forEach(comment => {
+                    const content = comment.getText().trim();
+                    let match = dtsregex.exec(content);
+                    while (match != null){
+                        const matchtypes = match[1];
+                        const matches = matchtypes
+                            .trim()
+                            .replace(/[\r\n\*]/g,"") // remove newline and * characters
+                            .split(",")
+                            .map(m => m.trim())
+                            .filter(m => types.indexOf(m) === -1);
+
+                        types = types.concat(matches);
+                        match = dtsregex.exec(content);
+                    }
+                });
+            });
+
+    return types;
 }
